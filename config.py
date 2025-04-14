a5fb034e505bb98bc2acb640471bea25537c7ce8
Antrax <fredcox@gmail.com>
2025-04-14 20:30:18 -0300

    ok

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
+from ast import arg
 import os
 from confz import BaseConfig, FileSource
+import argparse
+import sys
 
 
-DEBUG = False
-
-if DEBUG:
-    fileSource = FileSource(file="config.yaml")
-else:
-    HOME_DIR = os.path.expanduser("~")
-    CONFIG_DIR = os.path.join(HOME_DIR, ".config", "pwrmenu")
-    fileSource = FileSource(file=os.path.join(CONFIG_DIR, "./config.yaml"))
+VERSION = "0.1.2"
+CONFIG_FILE: str = os.path.join(
+    os.path.expanduser("~"), ".config", "pwrmenu", "config.yaml"
+)
+fileSource = FileSource(file=CONFIG_FILE)
 
 
 class MainWindow(BaseConfig):
@@ -31,19 +31,42 @@ class AppConfig(BaseConfig):
     MainWindow: MainWindow
 
 
-def printAsciiArt():
-    asciiArt = """
-    ██████╗ ██╗   ██╗███████╗██████╗ ███████╗██████╗ ██╗   ██╗
-    ██╔══██╗██║   ██║██╔════╝██╔══██╗██╔════╝██╔══██╗╚██╗ ██╔╝
-    ██████╔╝██║   ██║█████╗  ██████╔╝█████╗  ██████╔╝ ╚████╔╝
-    ██╔══██╗██║   ██║██╔══╝  ██╔══██╗██╔══╝  ██╔══██╗  ╚██╔╝
-    ██║  ██║╚██████╔╝███████╗██║  ██║███████╗██║  ██║   ██║
-    ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝   ╚═╝
+def printAsciiArt() -> None:
+    asciiArt = r"""
+_ ____      ___ __ _ __ ___   ___ _ __  _   _ 
+| '_ \ \ /\ / / '__| '_ ` _ \ / _ \ '_ \| | | |
+| |_) \ V  V /| |  | | | | | |  __/ | | | |_| |
+| .__/ \_/\_/ |_|  |_| |_| |_|\___|_| |_|\__,_|
+|_|    
     """
     print(asciiArt)
 
 
+def passArgs() -> None:
+    # Configuração do parser
+    parser = argparse.ArgumentParser(
+        description=f"pwrmenu - A Modern Power Menu for Hyprland. Version: {VERSION}.",
+    )
+
+    # Argumentos opcionais
+    parser.add_argument(
+        "-c",
+        "--config",
+        type=str,
+        # default=CONFIG_FILE,
+        required=False,
+        help="Path to the config file (config.yaml)",
+    )
+
+    # Processamento dos argumentos
+    args: argparse.Namespace = parser.parse_args()
+
+    if args.config != CONFIG_FILE:
+        CONFIG_FILE = args.config
+
+    print(f"Config file: {CONFIG_FILE}")
+
+
 if __name__ == "__main__":
-    config = AppConfig()
-    print(config)
     printAsciiArt()
+    passArgs()
