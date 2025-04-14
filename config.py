49c821434eb8b6429a8e85e7368a9d8a0067428c
Antrax <fredcox@gmail.com>
2025-04-14 13:57:20 -0300

    ok

--- config.py
+++ config.py
@@ -8,18 +8,12 @@ if DEBUG:
     HOME_DIR = os.path.expanduser("~")
     CONFIG_DIR = os.path.join(HOME_DIR, ".config", "power-menu")
 else:
-    CONFIG_DIR = "./"
+    CONFIG_DIR = "."
 
 
 class AppConfig(BaseConfig):
-    CONFIG_SOURCES = FileSource(file=os.path.join(CONFIG_DIR, "config.yaml"))
-    buttonForegroundColor: str
-    buttonBackgroundColor: str
-    buttonHoverColor: str
-    buttonFocusColor: str
-    buttonBorderColor: str
-    buttonBorderHoverColor: str
-    buttonBorderFocusColor: str
+    CONFIG_SOURCES = FileSource(file="./config.yaml")
+    colorButton: str
 
 
 if __name__ == "__main__":
