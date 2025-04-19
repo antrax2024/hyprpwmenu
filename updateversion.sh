06a5aa97602064871534c2b3065068fe057f4787
Antrax <fredcox@gmail.com>
2025-04-19 12:37:36 -0300

    changing all

--- updateversion.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-MINOR_VERSION="$(git rev-list --count HEAD)"
-MAJOR_VERSION="$(git rev-parse --short HEAD)"
-
-echo "$MINOR_VERSION.$MAJOR_VERSION"
-
-echo "$MINOR_VERSION.$MAJOR_VERSION" >version.txt
